de1652904987e66e52c1ada891011b080d951696
nath-btx <55541865+nath-btx@users.noreply.github.com>
2019-10-07 15:48:26 +0200

    Add files via upload

---
+++ PPE.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta name="Description" content="Put your description here.">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>Parking</title>
+  <script>
+  function myFunction() {
+    alert("coucou");
+  }
+  </script>
+</head>
+<body>
+
+  <header>
+    <h1> PPE </h1>
+  </header>
+
+  <main>
+    <section>
+      <form action="/action_page.php">
+        First name: <input type="text" name="fname"><br>
+        Last name: <input type="text" name="lname"><br>
+        <input type="submit" value="Submit">
+</form>
+      <p> COUCOU</p>
+      <button onclick="myFunction()">Click me</button>
+    </section>
+  </main>
+  <footer>
+    <p>
+      &copy; NB &amp; BG - <strong> 2019</strong>
+    </p>
+  </footer>
+</body>
+</html>
